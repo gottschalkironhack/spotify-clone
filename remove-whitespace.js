68dca612a2dd1810b7f76ca47aa2400f9650da08
Julia Gottschalk <giulia.gottschalk@gmail.com>
2017-05-12 19:52:01 +0200

    done

---
+++ remove-whitespace.js
@@ -0,0 +1,12 @@
+
+
+document.addEventListener("DOMContentLoaded", function(event)
+{
+  //var test = document.body.innerHTML.replace( /(>)[ \n\t]+/g, ">" );
+  document.body.innerHTML = document.body.innerHTML.replace( /(>)[ \n\t]+/g, ">" );
+  //document.body.innerHTML= document.body.innerHTML.replace( / /g," " );
+  //var innerhtml=document.body.innerHTML;
+  //document.body.innerHTML=document.body.innerHTML.replace(/\s+/g, ' ');
+  //console.log(test);
+
+});
